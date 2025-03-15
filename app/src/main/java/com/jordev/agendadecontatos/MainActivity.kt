144aca8895bddb5a53a38d4469e0c0d070cc7de4
Jorge Menezes <jorgedimendes@hotmail.com>
2025-03-14 21:34:52 -0300

    Removi os codigos padrao para começar codar

--- app/src/main/java/com/jordev/agendadecontatos/MainActivity.kt
+++ app/src/main/java/com/jordev/agendadecontatos/MainActivity.kt
@@ -18,30 +18,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AgendaDeContatosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun teste(){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AgendaDeContatosTheme {
-        Greeting("Android")
-    }
 }
